ac166e95d9516a242b4976ee157c71e6744c2ce3
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-04-25 09:04:09 +0530

    datamodel added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:coffee_masters/offerspage.dart';
+import 'package:coffee_masters/pages/menupage.dart';
+import 'package:coffee_masters/pages/offerspage.dart';
+import 'package:coffee_masters/pages/orderpage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -90,13 +92,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
     switch (selectedIndex) {
       case 0:
-        currentWidgetPage = const Text("Menu Page");
+        currentWidgetPage = const MenuPage();
         break;
       case 1:
         currentWidgetPage = const OffersPage();
         break;
       case 2:
-        currentWidgetPage = const Text("Orders Page");
+        currentWidgetPage = const OrderPage();
         break;
     }
 
