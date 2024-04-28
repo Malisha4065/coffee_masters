5882fe27f833cb8f923641cade34e0b2b4bd6929
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-04-28 22:20:55 +0530

    menu page updated

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -7,7 +7,7 @@ class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var p = Product(id: 1, name: "Dummy Product", price: 1.25, image: "");
-    return ProductItem(p);
+    return ProductItem(product: p);
   }
 }
 
@@ -22,8 +22,24 @@ class ProductItem extends StatelessWidget {
       padding: const EdgeInsets.all(8),
       child: Card(
         elevation: 4,
-        child: Text(product.name),
-      )
-    )
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("images/black_coffee.png"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                product.name,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("\$${product.price}"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
