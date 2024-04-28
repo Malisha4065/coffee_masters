e8ff7e617142a8c188faf91682171214f28d3ab9
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-04-28 22:57:32 +0530

    menupage improved

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -7,14 +7,30 @@ class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var p = Product(id: 1, name: "Dummy Product", price: 1.25, image: "");
-    return ProductItem(product: p);
+    var q =
+        Product(id: 2, name: "Another dummy Product", price: 1.25, image: "");
+    var r = Product(
+        id: 3,
+        name: "Different dummy Product much larger",
+        price: 1.25,
+        image: "");
+
+    return ListView(
+      children: [
+        ProductItem(product: p, onAdd: (){},),
+        ProductItem(product: q, onAdd: (){},),
+        ProductItem(product: r, onAdd: (){},),
+      ],
+    );
   }
 }
 
 class ProductItem extends StatelessWidget {
   final Product product;
+  final Function onAdd;
 
-  const ProductItem({super.key, required this.product});
+  const ProductItem({super.key, required this.product,
+      required this.onAdd});
 
   @override
   Widget build(BuildContext context) {
@@ -23,19 +39,39 @@ class ProductItem extends StatelessWidget {
       child: Card(
         elevation: 4,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset("images/black_coffee.png"),
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                product.name,
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              ),
+              padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+              child: Image.asset("images/black_coffee.png"),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("\$${product.price}"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        product.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("\$${product.price}"),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: ElevatedButton(
+                    onPressed: () {onAdd(product)},
+                    child: const Text("Add"),
+                  ),
+                )
+              ],
             ),
           ],
         ),
